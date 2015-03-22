ed93b3040f685100066aabe144e02b2f7335d022
Axel Tetzlaff <axel.tetzlaff@gmx.de>
2015-03-22 15:20:40 +0100

    dynamic item rendering

--- app/helpers/carendar/carendar_helper.rb
+++ app/helpers/carendar/carendar_helper.rb
@@ -1,7 +1,7 @@
 module Carendar
   module CarendarHelper
-    def carendar(calendar)
-      render partial: 'carendar/carendar', locals: {calendar: calendar}
+    def carendar(calendar, options = {}, &blk)
+      render partial: 'carendar/carendar', locals: {calendar: calendar, options: options, block: blk}
     end
   end
 end
